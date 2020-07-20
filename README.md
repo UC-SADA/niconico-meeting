2094d5222e3bc47e967cc532234bd51917e909f0
UC-SADA <uc.sadamitsu@gmail.com>
2020-07-21 06:14:46 +0900

    builder add

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# niconico-meeting
+# niconico-Display
+
+[NicoNico SPEENYA](https://github.com/chimerast/niconico-speenya)を参考に作成した[herokuアプリ](https://nico-chat.herokuapp.com/controller/)を作ったので、それをelectronの透明スクリーンを使ってデスクトップ上に表示させるソースを書いてみました。
+
+### 動作環境
+とりあえず、どんなもんか試してみたい方は
+* [windows 64bit用　パッケージファイル](https://github.com/UC-SADA/niconico-Display_win64/archive/master.zip)
+* [mac用　パッケージファイル](https://github.com/UC-SADA/niconico-Display_win64/archive/master.zip)
+### 動作環境
+
+* windows 7 64bit
+* node.js v12.1.0
+* electron 5.0.1
+
+### 動かし方
+
+node.js　と　electronをPCにインストール。
+
+本ソースコードを保存したディレクトリまで移動し下記コマンドを実行。
+
+```bash
+$ electron .
+```
+
+好きな「ルーム名」を入力。
+
+「透明ディスプレー」と「コントローラー」が表示されます。
+
+「コントローラー」上の「いいね」等を押すとスタンプ表示、コメントを記入する弾幕が流れます。
+
+また、QR-codeを読み取ればスマホからでもアクセス可能です。
+
+閉じるときは開いているファイルを閉じればOK。
+
+WEB会議や、プレゼンテーションで使うと今までとは一味違ったコミュニケーションを楽しめますよ。
+
+
+### 参考にしたサイトは
+
+[ニコニコメソッドプレゼンを全社会議に取り入れてみたら会議が面白くなった](http://tech.uzabase.com/entry/2015/06/01/143202)
+
+[社内プレゼン大会にニコニコメソッドを取り入れてみた話](http://atoms.loftwork.jp/20170925_nicomethod/)
+
+
+### 続き
+
+ローカルサーバーで試したいという方は、[niconico-Display2](https://github.com/UC-SADA/niconico-Display2)をご利用ください。
---
+++ index.js
@@ -0,0 +1,71 @@
+"use strict";
+const electron = require('electron');
+const config = require('config');
+const elc_app = electron.app;
+const elc_BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
+let inputWindow;
+let loginWindow;
+let isLoginWindow = false;
+
+// ログイン要求時に発火するイベント
+
+elc_app.on("login", (event, webContents, request, authInfo, callback)=>{
+
+    // プロキシサーバーからの要求だったら続行
+    if(authInfo.isProxy){
+
+        // 重複発火対策
+        if(!isLoginWindow){
+            isLoginWindow = true;
+        }
+        else{
+            return;
+        }
+
+        // 認証情報が送信されるまで待機
+        event.preventDefault();
+        loginWindow = new electron.BrowserWindow({
+            width: 300,
+            height: 180,
+            resizable: false,
+            webPreferences: {
+              nodeIntegration: true
+            }
+        });
+        loginWindow.setAlwaysOnTop(true);
+        // 入力ウィンドウをロード
+        loginWindow.setMenu(null);
+        loginWindow.loadURL('file://' + __dirname + '/login.html');
+
+        // IPCチャネル"proxy-auth"で受信待機
+        ipcMain.on("proxy-auth", (event, username, password)=>{
+
+            // 受信した認証情報をプロキシサーバーへ転送
+            callback(username, password);
+            isLoginWindow = false;
+        });
+    }
+});
+elc_app.on('ready', function (event) {
+  console.log(config);
+  var conf = "?";
+  for (var i = 0;i<Object.keys(config).length;i++){
+    conf += Object.keys(config)[i] + "=" + Object.values(config)[i] + "&"
+  }
+  inputWindow = new electron.BrowserWindow({
+      width: 300,
+      height: 180,
+      webPreferences: {
+        nodeIntegration: true
+      }
+  });
+  inputWindow.setAlwaysOnTop(true);
+//  inputWindow.setMenu(null);
+//  inputWindow.openDevTools();
+  if(config.testmode == 1){
+    inputWindow.loadURL('http://localhost:2525/start'+conf);  
+  }else{
+    inputWindow.loadURL('https://niconico-reaction.herokuapp.com/start'+conf);
+  }
+});
